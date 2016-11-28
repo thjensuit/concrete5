848179b19c87c8ba17120c88298d4dabbc53efae
Kien Vu <vu.kien@mulodo.com>
2016-11-28 15:02:53 +0700

    first commit

---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/9348cb8eeb000b2b/99cb5e4e3c473611.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_23a2157e1ede8728c910311cf4b16b56_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1480733954;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1480319293;
